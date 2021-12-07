2ecdd481e108ff8729dbb19020c0306db5e1e311
Namratha Sree <40785130+namrathasree@users.noreply.github.com>
2021-12-07 10:41:45 +0530

    Delete AreaofRectangleTDD directory

--- AreaofRectangleTDD/src/main/java/Rectangle.java
+++
@@ -1,9 +0,0 @@
-public class Rectangle {
-    public double area(double l,double b)
-    {
-        if(l==b) { throw new ArithmeticException("Length and Breadth cannot be equal"); }
-        if(l<=0) { throw new ArithmeticException("Length cannot be zero or negative"); }
-        if(b<=0) { throw new ArithmeticException("Breadth cannot be zero or negative"); }
-        return l*b;
-    }
-}
