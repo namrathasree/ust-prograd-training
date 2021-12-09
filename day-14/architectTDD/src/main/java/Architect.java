f1eb49ef3fcb62db89ee3deb33291dfb870b183c
Namrathasree <namrathasree3@gmail.com>
2021-12-09 17:04:40 +0530

    Addition and subtraction in architect TDD

--- day-14/architectTDD/src/main/java/Architect.java
+++ day-14/architectTDD/src/main/java/Architect.java
@@ -8,6 +8,17 @@ public class Architect {
         }
         this.value=value;
     }
+    public double magnitude1;
+    public double magnitude2;
+    public Architect(double magnitude1,double magnitude2)
+    {
+        if(magnitude1<=0 || magnitude2<=0)
+        {
+            throw new NullPointerException("Magnitude cannot be zero or negative");
+        }
+        this.magnitude1=magnitude1;
+        this.magnitude2=magnitude2;
+    }
 
     public double meterToCentimeter()
     {
@@ -20,5 +31,18 @@ public class Architect {
     {
         return value;
     }
+    public double addingOfMeterAndCentimeterGivesMeter(){
+        return magnitude1+(magnitude2/100);
 
+    }
+    public double addingOfCentimeterAndKilometerGivesCentimeter(){
+        return magnitude1+(magnitude2*100000);
+    }
+    public double subtractingMeterAndCentimeterGivesMeter(){
+        return magnitude1-(magnitude2/100);
+    }
+
+    public double subtractingCentimeterAndMetereterGivesCentieter() {
+        return magnitude1-(magnitude2*100);
+    }
 }
--- day-14/architectTDD/src/test/java/ArchitectTest.java
+++ day-14/architectTDD/src/test/java/ArchitectTest.java
@@ -7,7 +7,7 @@ public class ArchitectTest {
     class TestCentimeter{
 
         @Test
-        public void checkOneMeterIsEqualToCentimeter(){
+        public void checkOneMeterIsEqualToHundredCentimeter(){
             architect = new Architect(1);
             double actual= architect.meterToCentimeter();
             double expected=100;
@@ -16,7 +16,7 @@ public class ArchitectTest {
         }
 
         @Test
-        public void checkHundredCentimeterIsEqualToZeroZeroZeroOneKilometer(){
+        public void checkHundredCentimeterIsEqualToPointZeroZeroOneKilometer(){
             architect = new Architect(100);
             double actual= architect.centimeterToKilometer();
             double expected=0.001;
@@ -41,6 +41,45 @@ public class ArchitectTest {
 
             assertThrows(IllegalArgumentException.class, () -> new Architect(-1));
         }
+
+    }
+
+    @Nested
+    class AddingOfMagnitudes{
+
+        @Test
+        public void checkIfAdditionOfOneMeterAndHundredCentimetersGivesTwoMeters() {
+            architect =new Architect(1,100);
+            double actualMagnitude = architect.addingOfMeterAndCentimeterGivesMeter();
+            double expectedMagnitude = 2;
+            assertEquals(expectedMagnitude,actualMagnitude);
+        }
+        @Test
+        public void checkIfAdditionOfHundredCentimeterAndOneKilometerGivesOneLakhTwoHundredCentimeters() {
+            architect =new Architect(200,1);
+            double actualMagnitude = architect.addingOfCentimeterAndKilometerGivesCentimeter();
+            double expectedMagnitude = 100200;
+            assertEquals(expectedMagnitude,actualMagnitude);
+        }
+    }
+
+    @Nested
+    class SubtractingOfMagnitudes{
+        @Test
+        public void checkIfSubtractionOfOneMeterAndFiftyCentimeterGivesZeroPointFiveMeters(){
+            architect =new Architect(1,50);
+            double actualMagnitude = architect.subtractingMeterAndCentimeterGivesMeter();
+            double expectedMagnitude =0.5;
+            assertEquals(expectedMagnitude,actualMagnitude);
+        }
+        @Test
+        public void checkIfSubtractionOfTwoThousandCentimeterAndOneMeterGivesNineteenHundredCentimeters(){
+            architect =new Architect(2000,1);
+            double actualMagnitude = architect.subtractingCentimeterAndMetereterGivesCentieter();
+            double expectedMagnitude =1900;
+            assertEquals(expectedMagnitude,actualMagnitude);
+        }
+
     }
 
 }
