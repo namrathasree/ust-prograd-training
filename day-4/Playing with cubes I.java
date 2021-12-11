9f6e2bef58140f7c79f8cea65678e0201aa44281
Namrathasree <namrathasree3@gmail.com>
2021-12-11 19:18:43 +0530

    Day 4 bank and codewars java programs

---
+++ day-4/Playing with cubes I.java
@@ -0,0 +1,10 @@
+public class Cube{
+    // note: use primitive data type for the side of the cube
+  private int Side;
+  public int getSide(){
+    return this.Side;
+  }
+  public void setSide(int num){
+    this.Side =num;
+  }
+}
